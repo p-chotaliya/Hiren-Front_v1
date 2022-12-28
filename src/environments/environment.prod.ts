2a9476bd2216467a5e74806463f3e7d87b1c9ca2
parth chotaliya <42476875+p-chotaliya@users.noreply.github.com>
2022-12-28 12:43:33 -0500

    change environment setting to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // api_url: 'http://phpstack-192394-1377049.cloudwaysapps.com/api',
-  api_url: 'http://127.0.0.1:4000',
+  api_url: 'http://phpstack-192394-1377049.cloudwaysapps.com/api',
+  // api_url: 'http://127.0.0.1:4000',
   php_api_url: 'http://localhost/MyimageSaver',
   //prod_api_url: '  http://phpstack-192394-1377049.cloudwaysapps.com/api '
   // ', http://localhost/MyimageSaverhttp://phpstack-192394-571052.cloudwaysapps.com
